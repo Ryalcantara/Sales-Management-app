fb2b09880fc9b0013a911c8d46a6ead3546dd2cb
Roy <95271521+Ryotard@users.noreply.github.com>
2023-10-01 16:27:20 +0800

    update readme and employee profile

--- resources/views/components/employee-profile.blade.php
+++ resources/views/components/employee-profile.blade.php
@@ -25,7 +25,7 @@
                      </div>
                      <div class="flex flex-col items-center pb-10">
                          <img class="w-24 h-24 mb-3 rounded-full shadow-lg"
-                             src="/docs/images/people/profile-picture-3.jpg" alt="profile" />
+                             src="{{ asset('img/dog.jpg') }}" alt="profile" />
                          <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">{{ $employee->name }}</h5>
                          <span class="text-sm text-gray-500 dark:text-gray-400">{{ $employee->role }}</span>
 
