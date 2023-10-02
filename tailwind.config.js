4c7e5d000d7f3923972c70f16faf818acdf37c24
Roy <95271521+Ryotard@users.noreply.github.com>
2023-10-02 19:05:37 +0800

    fixed tailwind error

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,13 +13,6 @@ export default {
   },
   plugins: [
     require('flowbite/plugin'),
-    require('@catppuccin/tailwindcss')({
-      // prefix to use, e.g. `text-pink` becomes `text-ctp-pink`.
-      // default is `false`, which means no prefix
-      prefix: 'ctp',
-      // which flavour of colours to use by default, in the `:root`
-      defaultFlavour: 'latte'
-    }),
   ],
     darkMode: 'class',
 }
